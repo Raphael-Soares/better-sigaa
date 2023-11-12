829af206ce3b5816959bf41773767d51221316d9
Raphael-Soares <soaresphaell@gmail.com>
2023-11-11 21:31:52 -0300

    asdasd

---
+++ views/portal-docente.js
@@ -0,0 +1,6 @@
+const url = window.location.href;
+const portal_urls = 'sigaa/portais/discente/discente.jsf';
+
+if (url.includes(portal_urls)) {
+  const perfil = document.querySelector('#perfil-docente');
+}
