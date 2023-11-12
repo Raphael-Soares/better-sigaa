69dbe74b0f835c7f1599f434b951b26ac5c7efdf
Raphael-Soares <soaresphaell@gmail.com>
2023-11-12 10:54:28 -0300

    ajutes de estrutura

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log('Better Sigaa');
+
+const unidadeClas = document.querySelector('.unidade');
+
+const unidade = (unidadeClas.innerHTML =
+  "Extensão feita por <a  href='https://github.com/Raphael-Soares' target='_blank'>Raphael Soares</a> ");
--- views/portal-docente.js
+++
@@ -1,6 +0,0 @@
-const url = window.location.href;
-const portal_urls = 'sigaa/portais/discente/discente.jsf';
-
-if (url.includes(portal_urls)) {
-  const perfil = document.querySelector('#perfil-docente');
-}
