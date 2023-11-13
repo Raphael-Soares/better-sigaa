d99670c39cd2ace354678d3419363850fc8adaa7
Raphael-Soares <soaresphaell@gmail.com>
2023-11-12 21:53:02 -0300

    indo la

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ console.log('Better Sigaa');
 //   "Extensão feita por <a  href='https://github.com/Raphael-Soares' target='_blank'>Raphael Soares</a> ";
 
 document.querySelector('#perfil-docente').innerHTML += ` 
-
 <div class="INJECTED_LINKS">
   <a href="https://ensino.ifc.edu.br/calendarios-academicos/">Calendário Acadêmico</a>
   <a href="https://ensino.araquari.ifc.edu.br/horario-academico/">Horário Acadêmico</a>
@@ -13,15 +12,10 @@ document.querySelector('#perfil-docente').innerHTML += `
   <a href="perfil.jsf">Atualizar Foto e Perfil</a>
   <a href="/sigaa/abrirCaixaPostal.jsf?sistema=2">Mensagens</a>
 </div>
-
-
 `;
 
 const userInfo = document.querySelector('#info-usuario');
-
 const fotoPerfil = document.querySelector('.foto');
 const mudarSenha = document.querySelector('#mudar-senha');
-
 document.querySelector('.pessoal-docente ul').append(document.querySelector('.dados-pessoais'));
-
 document.querySelector('#painel-usuario').append(fotoPerfil);
